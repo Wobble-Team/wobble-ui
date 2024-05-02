fe834ba52b38464d45cdda3e274be3564316c744
Blake U <74560919+Blake254@users.noreply.github.com>
2024-05-02 10:14:38 -0400

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -4,8 +4,8 @@ const nextConfig = {
   async rewrites() {
     return [
       {
-        source: '/api/:path*',
-        destination: 'http://localhost:5000/:path*',
+        source: "/api/:path*",
+        destination: "http://localhost:8456/:path*",
       },
     ];
   },
