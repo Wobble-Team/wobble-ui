0f408a077723c4202dcdd47060219a3cf4bc7659
Peterson Ngo <petersonngo@Petersons-MBA.local>
2024-05-09 22:22:23 -0500

    grabs uri from backend

--- src/containers/home-page/welcome/index.tsx
+++ src/containers/home-page/welcome/index.tsx
@@ -1,10 +1,24 @@
 
 import { Button } from "@mui/material";
-import React, { FC } from "react";
+import React, { FC, useState } from "react";
+import axios from 'axios';
 
 interface WelcomeProps {}
 
 const Welcome: FC<WelcomeProps> = ({}) => {
+
+    const handleSpotifyAuth = async () => {
+      try {
+
+        const response = await axios.get('http://localhost:8456/auth/login');
+        window.location.href = response.data;
+      } catch (error) {
+        // Handle errors
+        console.error('Error fetching data from backend:', error);
+      }
+    }
+
+
   return (
     <section className="w-full flex justify-center">
       <div className=" flex flex-col py-10 md:py-28 items-center  gap-8 md:gap-24 w-full max-w-screen-xl mx-5 md:mx-20">
@@ -16,7 +30,8 @@ const Welcome: FC<WelcomeProps> = ({}) => {
           </div>
           <div>
             <p className="text-textColor text-sm md:text-lg">
-              We love music and stats. If you do too <Button variant="text">sign in</Button>
+              {/* We love music and stats. If you do too <Button variant="text" onClick={login}>sign in</Button> */}
+              We love music and stats. If you do too <Button variant="text" onClick={handleSpotifyAuth}>Login into Spotify</Button>
             </p>
           </div>
         </div>
